fb0a39484d8ec6b36bf6a9f320ffb2ad86a3875e
Lawrence <lawrencehan94@gmail.com>
2017-09-19 16:13:18 -0700

    v2

--- controller/uploadVC.swift
+++ controller/uploadVC.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Firebase
 
 class uploadVC: UIViewController {
 
@@ -6,4 +7,30 @@ class uploadVC: UIViewController {
   @IBOutlet weak var: selectImageButton: UIButton!
   @IBOutlet weak var: postButton: UIButton!
   
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    picker.delegate = self
+  }
+  
+  @IBAction func selectImage (_ sender: Any) {
+    presentImagePicker()
+  }
+  
+  @IBAction func createPost (_ sender: Any) {
+  } 
+}
+
+// IMAGE PICKER
+extension uploadVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+  func presentImagePicker() {
+    picker.allowsEditing
+    picker.sourceType = .photoLibrary
+  }
+  
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String, Any]) {
+    if let image = info[UIImagepickerControllerEditedImage] as? UIImage {
+      self.previewImage.image = image
+    }
+  }
 }
