92a9c8bd770a320e3005a8e3e2546e0e621d518e
Lawrence <lawrencehan94@gmail.com>
2017-09-19 16:06:33 -0700

    v1

---
+++ controller/uploadVC.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class uploadVC: UIViewController {
+
+  @IBOutlet weak var: previewImage: UIImageView!
+  @IBOutlet weak var: selectImageButton: UIButton!
+  @IBOutlet weak var: postButton: UIButton!
+  
+}
