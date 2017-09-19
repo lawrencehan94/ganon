df6ba1ed17cb4b1d678917849150a669f97e6114
Lawrence <lawrencehan94@gmail.com>
2017-09-19 16:44:02 -0700

    v3

--- controller/uploadVC.swift
+++ controller/uploadVC.swift
@@ -34,3 +34,39 @@ extension uploadVC: UIImagePickerControllerDelegate, UINavigationControllerDeleg
     }
   }
 }
+
+// Upload Post to Firebase (you also need to create a storage reference so you can store the image)
+extension uploadVC {
+  
+  func uploadPost(){
+    let userId = Auth.auth()!.currentUser!.uid
+    let databaseReference = Database.database().reference()
+    let storageReference = Storage.storage().reference(forURL: "gs://chat-box-6f8eb.appspot.com // or string from firebase storage")
+   
+    let postId = databaseReference.child("posts").childByAutoId().key // in database we are creating a new posts folder, childByAutoID organizes the posts, .key gives you a string
+    let imageReference = storageReference.child("posts").child(userId).child("\(postId).jpg") // you want to store the posts under the user's folder
+    
+    //upload image by converting the image to a data
+    let imageData = UIImageJPEGRepresentation(self.previewImage.image, 0.6) //0.6 is the compression size
+    let uploadImageData = imageReference.put(imageData!, metadata: nil) { (metadata, error) in
+      if error != nil {
+        print(error!.localizedDescription)
+        return
+      } 
+      imageReference.downloadURL(completion: { (url, error) in
+        if let url = url {
+          let feed = ["userID": userId, 
+                      "pathToImage": url.absoluteString, 
+                      "likes": 0, 
+                      "author": Auth.auth()?.currentUser!.displayName!,
+                      "postID": postId] as [String: Any]
+          let postFeed = ["\(userId)": feed]
+          databaseReference.child("posts").updateChildValues(postFeed) //you're uploading a dictionary back to firebase's dictionary
+          self.dismiss(animated: true, completion: nil) //dismiss image picker controller
+        }
+      
+    }
+  }
+
+}
+
