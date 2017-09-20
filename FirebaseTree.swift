0a47308133ea15500a83a578e77e09d5efc259db
Lawrence <lawrencehan94@gmail.com>
2017-09-20 15:52:42 -0700

    v1

---
+++ FirebaseTree.swift
@@ -0,0 +1,27 @@
+Users following each other
+
+local-edition-e6ca4
+
+- Users
+
+  - lawrencehan94 ("0O5TDkOMKrf5Cw0v98PyOjqZQG32")
+    - followers
+    - following
+      - lawrencehan94 is following julian ("KuVhM0Z_rxk_Q1kp0mR"): julian ("mbH5CzoIFUS8ij8aD4mZNiWXai32")
+      - lawrencehan94 is following tomford("KuVhMNc8vJQPPasgQTb"): tomford ("q1kyDki80tevb0aElAHVlExQzk53")
+    - urlToImage; "https://firebasestorage.googleapis.com/v0/b/local-edition-e6ca4.appspot.com/o/users%2F0O5TDkOMKrf5Cw0v98PyOjqZQG32.jpg?alt=media&token=3378a395-0fcc-4ecd-bb31-8fca7f044826"
+    - userId: "0O5TDkOMKrf5Cw0v98PyOjqZQG32"
+    - username: "lawrencehan94"
+  
+  - bobmarley ("J7QtDfEvjJa7IbtSZtOCiNWjl5B2")
+
+- Posts
+  
+  - lawrencehan94 ("0O5TDkOMKrf5Cw0v98PyOjqZQG32")
+    - author: "lawrencehan94"
+    - caption: ""
+    - likes: 0
+    - pathToImage: "https://firebasestorage.googleapis.com/v0/b/local-edition-e6ca4.appspot.com/o/posts%2F0O5TDkOMKrf5Cw0v98PyOjqZQG32%2F-KuTnNghHVDhPWeW1FLg.jpg?alt=media&token=173242b7-6e26-4736-8f35-7a31c6d27d17"
+    - postID: "-KuTnNghHVDhPWeW1FLg"
+    - userID: lawrencehan94 ("0O5TDkOMKrf5Cw0v98PyOjqZQG32") 
+  
