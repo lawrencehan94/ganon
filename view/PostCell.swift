63b1befcb7a479db9230a180b8abd43e687046b4
Lawrence <lawrencehan94@gmail.com>
2017-09-20 16:31:53 -0700

    v1

---
+++ view/PostCell.swift
@@ -0,0 +1,14 @@
+class PostCell: UITableViewCell {
+  @IBOutlet weak var postImage: UIImageView!
+  @IBOutlet weak var profilePhoto: UIImageView!
+  @IBOutlet weak var usernameLabel: UILabel!
+  @IBOutlet weak var captionLabel: UILabel!
+  @IBOutlet weak var likeCountLabel: UILabel!
+ 
+  likeButtonPressed () {
+  }
+  
+  unlikeButtonPressed(){
+  }
+  
+}
